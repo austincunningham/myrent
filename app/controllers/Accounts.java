8b0cbaf6f2be2255dac37b79a4114f2b01f14737
austincunningham <austincunningham@hotmail.com>
2016-06-13 22:37:09 +0100

    user model, authenticate and register

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -9,20 +9,52 @@ import models.*;
 
 public class Accounts extends Controller {
 
-    public static void index() {
-        render();
-    }
-    
-    public static void Signup() {
-        render();
-    }
-    public static void Login() {
-        render();
-    }
-    public static void Logout()
-	  {
+	public static void index() {
+		render();
+	}
+
+	public static void Signup() {
+		render();
+	}
+
+	public static void Login() {
+		render();
+	}
+
+	public static void Logout() {
 		session.clear();
-	    Welcome.index();
-	  }
+		Welcome.index();
+	}
+
+	public static void register(String firstName, String lastName, String email, String password) {
+		Logger.info(firstName + " " + lastName + " " + email + " " + password);
+		User user = new User(firstName, lastName, email, password);
+		user.save();
+		Login();
+
+	}
+
+	public static void authenticate(String email, String password) {
+		Logger.info("Attempting to authenticate with " + email + ":" + password);
+		User user = User.findByEmail(email);
+		if ((user != null) && (user.checkPassword(password) == true)) {
+			Logger.info("Authentication successful");
+			session.put("logged_in_userid", user.id);
+			index();
+		} else {
+			Logger.info("Authentication failed");
+			Login();
+		}
+	}
+
+	public static User getCurrentUser() {
+		String userId = session.get("logged_in_userid");
+		if (userId == null) {
+			return null;
+		}
+		User logged_in_user = User.findById(Long.parseLong(userId));
+		Logger.info("Logged in User: " + logged_in_user.firstName);
+		return logged_in_user;
+	}
 
 }
