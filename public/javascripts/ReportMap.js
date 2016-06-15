aa29dfa70ed9d3eafe977aebd41f807917364cd9
austincunningham <austincunningham@hotmail.com>
2016-06-15 22:05:54 +0100

    Contact placeholder form, route and controller setup

--- public/javascripts/ReportMap.js
+++ public/javascripts/ReportMap.js
@@ -13,19 +13,19 @@ function initialize() {
   const center = new google.maps.LatLng(52.2514845,-7.1265728);
   const initRadius = 10000;
   
-//  const mapProp = {
-    const mapOptions = {
+  const mapProp = {
+//    const mapOptions = {
     center: center,
     zoom: 7,
     mapTypeId: google.maps.MapTypeId.ROAD
   };
   
-  //const mapDiv = document.getElementById('map_canvas');
-  //const map = new google.maps.Map(mapDiv, mapProp);
-  const map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  const mapDiv = document.getElementById('map-canvas');
+  const map = new google.maps.Map(mapDiv, mapProp);
+  //const map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   
-  //mapDiv.style.width = '500px';
-  //mapDiv.style.height = '500px';
+  mapDiv.style.width = '500px';
+  mapDiv.style.height = '500px';
 
   circle = new google.maps.Circle({
     center: center,
@@ -37,8 +37,9 @@ function initialize() {
     fillOpacity: 0.4,
     draggable: true,
   });
-  circle.setEditable(true);//allows constying radius be dragging anchor point
+  circle.setEditable(true);//allows radius be dragging anchor point
   circle.setMap(map);
+  map.setCenter(center);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
