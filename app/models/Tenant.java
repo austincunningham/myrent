98ed7bbf4c708ee42e3a0386c5d494c8151e11c4
austincunningham <austincunningham@hotmail.com>
2016-06-30 21:03:14 +0100

    Tenant model view created

--- app/models/Tenant.java
+++ app/models/Tenant.java
@@ -1,6 +1,33 @@
 package models;
+import javax.persistence.Entity;
 
-public class Tenant
+import play.db.jpa.Model;
+
+@Entity
+public class Tenant extends Model
 {
+  public String firstName;
+  public String lastName;
+  public String email;
+  public String password;
+  public String tenantReference;
+  
+  public Tenant(String firstName, String lastName, String email,String password, String tenantReference)
+  {
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.email     = email;
+    this.password  = password;
+    this.tenantReference = tenantReference;
+  }
+  
+  public static Tenant findByEmail(String email)
+  {
+    return find("email", email).first();
+  }
 
+  public boolean checkPassword(String password)
+  {
+    return this.password.equals(password);
+  }
 }
