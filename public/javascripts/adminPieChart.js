763b4e74aeace0fe268a54e2fe498f250fa1f70d
austincunningham <austincunningham@hotmail.com>
2016-07-10 08:41:52 +0100

    added a colume chart to Administrator Charts

--- public/javascripts/adminPieChart.js
+++ public/javascripts/adminPieChart.js
@@ -7,58 +7,111 @@
  */
 
 window.onload = function () {
-
-      //function loadChart() {
-      var chart = new CanvasJS.Chart('chartContainer',
-          {
-            title: {
-              text: 'Precentage of rental market revenu by Landlord',
-            },
-            data: [
-              {
-                type: 'pie',
-                toolTipContent: '{y} %',
-                dataPoints: [],
+        var chart = new CanvasJS.Chart('chartContainer',
+            {
+              title: {
+                text: '% Rental income by Landlord',
               },
-            ],
-          });
-
-      $(function loadChartData() {
-        $.get('/Administrator/dataCharts', function (data) {
-          $.each(data, function (index, chartData) {
-                console.log(chartData);
-              }
-          );
-          chartCallback(data);
-          $.each(chartSort, function (i, val) {
-                let j = 0;
-              }
-          );
-
-          let total = 0;
-          for (j = 0; j < chartSort.length; j++) {
-            total += parseFloat(chartSort[j][0]);
-          }
-
-          console.log('landlord ' + chartSort);
-          console.log('total rent :' + total);
-          let val;
-          let i = 0;
-          for (val of chartSort) {
-            console.log('Getting added to pie ' + chartSort[i][1]);
-            chart.options.data[0].dataPoints.push({
-              y: ((parseFloat(val[0]) / total) * 100),
-              indexLabel: val[1] + ' €' + val[0],
+              data: [
+                {
+                  type: 'pie',
+                  toolTipContent: '{y} %',
+                  dataPoints: [],
+                },
+              ],
             });
-            console.log(chart.options.data[0].dataPoints);
-            chart.render();
-          }
 
+        $(function loadChartData() {
+          $.get('/Administrator/dataCharts', function (data) {
+            $.each(data, function (index, chartData) {
+                  console.log(chartData);
+                }
+            );
+            chartCallback(data);
+            $.each(chartSort, function (i, val) {
+                  let j = 0;
+                }
+            );
+
+            let total = 0;
+            for (j = 0; j < chartSort.length; j++) {
+              total += parseFloat(chartSort[j][0]);
+            }
+
+            console.log('landlord ' + chartSort);
+            console.log('total rent :' + total);
+            let val;
+            let i = 0;
+            for (val of chartSort) {
+              console.log('Getting added to pie ' + chartSort[i][1]);
+              chart.options.data[0].dataPoints.push({
+                y: (Math.round((parseInt(val[0]) / total) * 100)),
+                indexLabel: val[1] + ' €' + val[0] + ' Percentage : '
+                + (Math.round((parseFloat(val[0] / total) * 100))) + '%',
+              });
+              console.log(chart.options.data[0].dataPoints);
+              chart.render();
+            }
+
+          });
         });
-      });
-    };
+      };
 
 function chartCallback(data) {
   chartSort = data;
   return chartSort;
 }
+
+function reloadPie() {
+  location.reload();
+}
+
+function loadChart() {
+  var chart = new CanvasJS.Chart('chartContainer',
+      {
+        title: {
+          text: '% Rental income by Landlord',
+        },
+        data: [
+          {
+            type: 'column',
+            toolTipContent: '{y} %',
+            dataPoints: [],
+          },
+        ],
+      });
+
+  $(function loadChartData() {
+    $.get('/Administrator/dataCharts', function (data) {
+      $.each(data, function (index, chartData) {
+            console.log(chartData);
+          }
+      );
+      chartCallback(data);
+      $.each(chartSort, function (i, val) {
+            let j = 0;
+          }
+      );
+
+      let total = 0;
+      for (j = 0; j < chartSort.length; j++) {
+        total += parseFloat(chartSort[j][0]);
+      }
+
+      console.log('landlord ' + chartSort);
+      console.log('total rent :' + total);
+      let val;
+      let i = 0;
+      for (val of chartSort) {
+        console.log('Getting added to pie ' + chartSort[i][1]);
+        chart.options.data[0].dataPoints.push({
+          y: (Math.round((parseInt(val[0]) / total) * 100)),
+          indexLabel: val[1] + ' €' + val[0],
+        });
+        console.log(chart.options.data[0].dataPoints);
+        chart.render();
+      }
+
+    });
+  });
+};
