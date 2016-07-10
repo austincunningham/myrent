8cf76b4ef4c55ad5b8808d90851f39c4efd3f42b
austincunningham <austincunningham@hotmail.com>
2016-07-10 14:14:57 +0100

    validation working for Administrator login

--- public/javascripts/adminLogin.js
+++ public/javascripts/adminLogin.js
@@ -1,27 +1,28 @@
-$('.ui.form').form({
-  fields: {
-    email: {
+$('.ui.form')
+    .form({
+      fields: {
+        email: {
           identifier: 'email',
           rules: [
             {
               type: 'empty',
-              prompt: 'Please enter your user name, It will be the email you signed up with',
+              prompt: 'Please enter your Administrator user name',
             },
           ],
         },
-    password: {
-      identifier: 'password',
-      rules: [
-        {
-          type: 'empty',
-          prompt: 'Please enter a password',
+        password: {
+          identifier: 'password',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter a password',
+            },
+            {
+              type: 'minLength[6]',
+              prompt: 'Your password is at least {ruleValue} characters',
+            },
+          ],
         },
-        {
-          type: 'minLength[6]',
-          prompt: 'Your password is at least {ruleValue} characters',
-        },
-      ],
-    },
-  },
-})
+      },
+    })
 ;
